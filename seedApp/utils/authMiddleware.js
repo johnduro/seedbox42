e242efe2a177160fb6a2ca762a754be8e8c58e56
johnduro <mle-roy@student.42.fr>
2015-09-16 20:26:31 +0200

    ajout de l appli nodejs/expressjs dans le dossier seedApp

---
+++ seedApp/utils/authMiddleware.js
@@ -0,0 +1,33 @@
+var jwt = require('jsonwebtoken');
+
+
+var authMiddleware = function (req, res, next) {
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	if (token)
+	{
+		jwt.verify(token, req.app.get('secret'), function (err, decoded)
+				   {
+					   if (err)
+					   {
+						   return res.json({ success: false, message: 'Failed to authenticate token.'});
+					   }
+					   else
+					   {
+						   req.user = decoded;
+						   next();
+					   }
+				   });
+	}
+	else
+	{
+		return res.status(403).send(
+			{
+				success: false,
+				message: 'No token provided.'
+			});
+	}
+};
+
+
+module.exports = authMiddleware;
