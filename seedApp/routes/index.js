6531b1b052eb129dfd8b4cc8010fa78b7c302ff2
rabid-on <rabid-on@student.42.fr>
2015-09-17 18:20:38 +0200

    angularjs ready, auth and upload torrent

--- seedApp/routes/index.js
+++ seedApp/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index.html');
-});
+//router.get('/', function(req, res) {
+ // res.render('index.html');
+//});
 
 module.exports = router;
