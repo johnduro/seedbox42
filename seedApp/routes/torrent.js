c4aa1b4d383a80f7d0a9e9f23db681406e2bd860
root <root@ns327732.ip-37-187-111.eu>
2015-09-17 17:36:28 +0200

    angularjs implementation

---
+++ seedApp/routes/torrent.js
@@ -0,0 +1,13 @@
+// var express = require('express');
+// var WebTorrent = require('webtorrent');
+// var router = express.Router();
+
+// router.post('/', function(req, res, next) {
+// 	var client = new WebTorrent();
+// 	var magnet = req.body.magnet;
+// 	client.add(magnet, function(torrent) {
+// 		client.destroy()
+// 	});
+// });
+
+// module.exports = router;
