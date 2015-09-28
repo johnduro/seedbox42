4354f420e1a47ec4d04adc9ba77b252353b816ca
Acollin <alexis-collin@hotmail.fr>
2015-09-28 11:40:52 +0200

    Ajoute de la connexion fictive TODO: relier a la db

--- seedApp/public/app/controllers/connexionCtrl.js
+++ seedApp/public/app/controllers/connexionCtrl.js
@@ -1,5 +1,12 @@
 
-app.controller('connexionCtrl', function ($scope, $rootScope) {
+app.controller('connexionCtrl', function ($scope, $rootScope, $state) {
 
 	console.log("connexionCtrl");
+
+	$scope.email = "test@hotmail.com";
+	$scope.password = "okok";
+
+	$scope.connexion = function(){
+		$state.go("seedbox.dashboard");
+	}
 });
