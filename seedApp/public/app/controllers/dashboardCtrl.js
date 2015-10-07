ce34b6e0242ecc7d9603ebdb86c78e2a43567402
Alexis Collin <Alexis@iMac-de-Alexis.local>
2015-10-07 17:42:50 +0200

    Ajout des socket dans angular

--- seedApp/public/app/controllers/dashboardCtrl.js
+++ seedApp/public/app/controllers/dashboardCtrl.js
@@ -1,5 +1,14 @@
 
-app.controller('dashboardCtrl', function ($scope, $rootScope) {
+app.controller('dashboardCtrl', function ($scope, $rootScope, socket) {
 
 	console.log("dashboardCtrl");
+
+	$scope.voteFor = function(choice){
+	  socket.emit('vote', {vote : choice })
+	}
+
+	socket.on('votes', function(msg){
+	  $scope.votes = msg.votes;
+	  $scope.$apply();
+	});
 });
