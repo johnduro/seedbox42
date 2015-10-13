95d1baeb139d78a89005fe2d0882ef34c768683d
johnduro <mle-roy@student.42.fr>
2015-10-13 05:54:56 +0200

    ajout-suppression des fichier en db a l ajout des torrent ok

--- seedApp/routes/torrent.js
+++ seedApp/routes/torrent.js
@@ -2,6 +2,7 @@ var express = require('express');
 var multer = require('multer');
 var router = express.Router();
 var io = require('socket.io');
+var File = require("../models/File.js");
 
 // ====================================
 // TORRENT UPLOADS
@@ -23,7 +24,19 @@ var torrentUpldHandler = multer({
 	}});
 // ************************************
 
-var tmp = "";
+
+// ====================================
+// CREATE FILE
+// ====================================
+var createFile = function (torrentAdded, userId) {
+	File.create({ name: torrentAdded['name'], creator: userId, hashString: torrentAdded['hashString']}, function (err, file) {
+		if (err)
+			throw err;
+	});
+};
+// ************************************
+
+// var tmp = "";
 
 router.post('/', function (req, res, next) {
 	// req.app.torrentClient.add(req.body.magnet, function(torrent) {
@@ -37,9 +50,19 @@ router.post('/', function (req, res, next) {
 router.post('/add-url', function (req, res, next) {
 	req.app.get('transmission').torrentAdd({filename: req.body.url}, function (err, resp) {
 		if (err)
-			res.json({success: false, message: 'torrent not added, wrong url'});
+			res.json({ success: false, message: 'torrent not added, wrong url' });
 		else
-			res.json({success: true, message: 'torrent successfully added'});
+		{
+			if ('torrent-duplicate' in resp)
+				res.json({ success: false, message: 'duplicate, torrent already present' });
+			else if ('torrent-added' in resp)
+			{
+				createFile(resp['torrent-added'], req.user._id);
+				res.json({ success: true, message: 'torrent successfully added' });
+			}
+			else
+				res.json({ success: false, message: 'unexpected error' });
+		}
 	});
 });
 
@@ -49,25 +72,51 @@ router.post('/add-torrents', torrentUpldHandler.array('torrent', 10), function(r
 	var counter = 0;
 	req.files.forEach(function (file) {
 		counter++;
-		req.app.get('transmission').torrentAdd({filename: process.cwd() + '/' + file.path}, function (err, resp) {
+		req.app.get('transmission').torrentAdd({ filename: process.cwd() + '/' + file.path }, function (err, resp) {
 			counter--;
 			if (err)
-				resAll.push({torrent: file.filename, success: false});
+				resAll.push({ torrent: file.filename, success: false });
 			else
-				resAll.push({torrent: file.filename, success: true});
+			{
+				if ('torrent-duplicate' in resp)
+					resAll.push({ success: false, message: 'duplicate, torrent already present', torrent: file.filename });
+				else if ('torrent-added' in resp)
+				{
+					createFile(resp['torrent-added'], req.user._id);
+					resAll.push({ success: true, message: 'torrent successfully added', torrent: file.filename });
+				}
+				else
+					resAll.push({ success: false, message: 'unexpected error', torrent: file.filename });
+			}
 			if (counter == 0)
 				res.json(resAll);
 		});
 	});
 });
 
+
 router.delete('/:id', function (req, res, next) {
 	var removeLocalData = (req.body.removeLocalData === "true");
-	req.app.get('transmission').torrentRemove(parseInt(req.params.id, 10), removeLocalData, function (err, resp) {
+	var id = parseInt(req.params.id, 10);
+	if (removeLocalData)
+	{
+		req.app.get('transmission').torrentGet(['hashString'], id, function (err, resp) {
+			if (err)
+				throw err;
+			else
+			{
+				File.findOneAndRemove({hashString: resp['torrents'][0]['hashString']}, function (err, file) {
+					if (err)
+						return next(err);
+				});
+			}
+		});
+	}
+	req.app.get('transmission').torrentRemove(id, removeLocalData, function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Could not remove torrent"});
+			res.json({ success: false, message: "Could not remove torrent" });
 		else
-			res.json({success: true, message: 'Torrent successfuly removed'});
+			res.json({ success: true, message: 'Torrent successfuly removed' });
 	});
 });
 
@@ -79,9 +128,9 @@ router.post('/move/:direction/:id?', function (req, res, next) {
 		ids = parseInt(req.params.id, 10);
 	req.app.get('transmission').queueMovementRequest('queue-move-' + req.params.direction, ids, function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Could not move torrent " + req.params.direction});
+			res.json({ success: false, message: "Could not move torrent " + req.params.direction });
 		else
-			res.json({success: true, message: 'Torrent successfuly moved ' + req.params.direction});
+			res.json({ success: true, message: 'Torrent successfuly moved ' + req.params.direction });
 	});
 });
 
@@ -93,27 +142,27 @@ router.post('/action/:action/:id?', function (req, res, next) {
 		ids = parseInt(req.params.id, 10);
 	req.app.get('transmission').torrentActionRequest('torrent-' + req.params.action, ids, function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Action: " + req.params.action + " failed"});
+			res.json({ success: false, message: "Action: " + req.params.action + " failed" });
 		else
-			res.json({success: true, message: "Action: " + req.params.action + " was a success"});
+			res.json({ success: true, message: "Action: " + req.params.action + " was a success" });
 	});
 });
 
 router.get('/session-stats', function (req, res, next) {
 	req.app.get('transmission').sessionStats(function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Could not get session stats"});
+			res.json({ success: false, message: "Could not get session stats" });
 		else
-			res.json({success: true, data: resp});
+			res.json({ success: true, data: resp });
 	});
 });
 
 router.get('/session-get', function (req, res, next) {
 	req.app.get('transmission').sessionGet(function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Could not get session detailled infos"});
+			res.json({ success: false, message: "Could not get session detailled infos" });
 		else
-			res.json({success: true, data: resp});
+			res.json({ success: true, data: resp });
 	});
 });
 
@@ -121,18 +170,18 @@ router.get('/session-get', function (req, res, next) {
 router.post('/blocklist-update', function (req, res, next) {
 	req.app.get('transmission').blocklistUpdate(function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Could not update blocklist"});
+			res.json({ success: false, message: "Could not update blocklist"} );
 		else
-			res.json({success: true, message: "Blocklist successfully updated", data: resp});
+			res.json({ success: true, message: "Blocklist successfully updated", data: resp} );
 	});
 });
 
 router.get('/port-test', function (req, res, next) {
 	req.app.get('transmission').portTest(function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Could not check port"});
+			res.json({ success: false, message: "Could not check port"} );
 		else
-			res.json({success: true, message: "Port checked", "port-is-open": resp["port-is-open"]});
+			res.json({ success: true, message: "Port checked", "port-is-open": resp["port-is-open"] });
 	});
 });
 
@@ -141,9 +190,9 @@ router.post('/session-shutdown', function (req, res, next) {
 	{
 		req.app.get('transmission').sessionClose(function (err, resp) {
 			if (err)
-				res.json({success: false, message: "Could not close session"});
+				res.json({ success: false, message: "Could not close session" });
 			else
-				res.json({success: true, message: "Session successfully closed"});
+				res.json({ success: true, message: "Session successfully closed" });
 		});
 	}
 	else
@@ -154,9 +203,9 @@ router.get('/get-all-torrents', function (req, res, next) {
 	var fields = ['uploadRatio', 'id', 'addedDate', 'isFinished', 'leftUntilDone', 'name', 'rateDownload', 'rateUpload', 'queuePosition', 'downloadDir', 'eta', 'peerConnected', 'percentDone', 'startDate', 'status', 'totalSize', 'torrentFile'];
 	req.app.get('transmission').torrentGet(fields, {}, function (err, resp) {
 		if (err)
-			res.json({success: false, message: "Could not get all torrents infos"});
+			res.json({ success: false, message: "Could not get all torrents infos" });
 		else
-			res.json({success: true, message: "Torrent infos ok", data: resp});
+			res.json({ success: true, message: "Torrent infos ok", data: resp });
 	});
 });
 
--- seedApp/routes/users.js
+++ seedApp/routes/users.js
@@ -38,52 +38,61 @@ router.get('/', function(req, res, next) {
 
 // router.post('/', avatarUpldHandler.single('avatar'), validateAvatar, function(req, res, next) {
 router.post('/', avatarUpldHandler.single('avatar'), function(req, res, next) {
-	if (req.user.role === 0 || req.user.login === req.body.login) {
+	if (req.user.role === 0 || req.user.login === req.body.login)
+	{
 		//req.body.avatar = req.file.path;
 		User.create(req.body, function(err, post) {
-			if (err) return next(err);
+			if (err)
+				return next(err);
 			User.find(function (err, users) {
-				if (err) return next(err);
+				if (err)
+					return next(err);
 				res.json({ success: true, message: 'user successfully created', data: users});
 			});
 			// res.json(post);
 		});
-	} else {
-		res.json({ success: false, message: "You don't have enought rights for this action"});
 	}
+	else
+		res.json({ success: false, message: "You don't have enought rights for this action"});
 });
 
 router.get('/:id', function(req, res, next) {
 	User.findById(req.params.id, function (err, post) {
-		if (err) return next(err);
+		if (err)
+			return next(err);
 		res.json(post);
 	});
 });
 
 router.put('/:id', function(req, res, next) {
-	if (req.user.login == req.body.login || req.user.role == 0) {
+	if (req.user.login == req.body.login || req.user.role == 0)
+	{
 		User.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-			if (err) return next(err);
+			if (err)
+				return next(err);
 			res.json(post);
 		});
-	} else {
-		res.json({ success: false, message: "You don't have enought rights for this action"});
 	}
+	else
+		res.json({ success: false, message: "You don't have enought rights for this action"});
 });
 
 router.delete('/:id', function(req, res, next) {
-	if (req.user.login == req.body.login || req.user.role == 0) {
+	if (req.user.login == req.body.login || req.user.role == 0)
+	{
 		User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-			if (err) return next(err);
+			if (err)
+				return next(err);
 			User.find(function (err, users) {
-				if (err) return next(err);
+				if (err)
+					return next(err);
 				res.json({ success: true, message: 'user successfully deleted', data: users});
 			});
 			//res.json(post);
 		});
-	} else {
-		res.json({ success: false, message: "You don't have enought rights for this action"});
 	}
+	else
+		res.json({ success: false, message: "You don't have enought rights for this action"});
 });
 
 module.exports = router;
