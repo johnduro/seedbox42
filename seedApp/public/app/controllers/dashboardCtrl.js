5314402f7bc2b6a183a8f039dca74a8ded3c3787
Alexis Collin <Alexis@iMac-de-Alexis.local>
2015-10-15 18:23:34 +0200

    Ajout du refresh torrent avec les sockets

--- seedApp/public/app/controllers/dashboardCtrl.js
+++ seedApp/public/app/controllers/dashboardCtrl.js
@@ -2,10 +2,10 @@
 app.controller('dashboardCtrl', function ($scope, $rootScope, socket, $timeout) {
 
 	console.log("dashboardCtrl");
-	socket.emit('connection');
+
 	$scope.connectedUsers = 0;
 
-	socket.on('connected-users', function(data){
+	socket.on('connectedUsers', function(data){
 		$timeout(function() {
 			$scope.connectedUsers = data.connectedUsers;
 		  	$scope.$apply();
