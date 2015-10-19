8e9c0f100e70fa93e7091e28ee27e606079d4ba0
Alexis COLLIN <alexis-collin@hotmail.fr>
2015-10-19 20:02:55 +0200

    Connexion ok

--- seedApp/public/app/controllers/connexionCtrl.js
+++ seedApp/public/app/controllers/connexionCtrl.js
@@ -11,7 +11,10 @@ app.controller('connexionCtrl', function ($scope, $rootScope, $state, RequestHan
 		RequestHandler.post(api + "authenticate", $scope.user)
 			.then(function(result){
 				if (result.data.success){
+					$rootScope.user = result.data.data;
 					localStorage.setItem("user", JSON.stringify(result.data.data));
+
+					$rootScope.token = result.data.token;
 					localStorage.setItem("token", result.data.token);
 					$state.go("seedbox.dashboard");
 				}
