e3d0624b80581e64e8a75d63f2b72ee1927d4c4b
Alexis Collin <Alexis@iMac-de-Alexis.local>
2015-10-20 10:28:46 +0200

    Ajout du path dans l'affichage + double click pour entrer dans le dossier

--- seedApp/public/app/controllers/filesCtrl.js
+++ seedApp/public/app/controllers/filesCtrl.js
@@ -1,4 +1,3 @@
-
 app.controller('filesCtrl', function ($scope, $rootScope, RequestHandler, socket, $timeout) {
 
 	console.log("filesCtrl");
@@ -6,12 +5,31 @@ app.controller('filesCtrl', function ($scope, $rootScope, RequestHandler, socket
 	$scope.treeBase = '';
 	$scope.treeSelected = '';
 	$scope.elementsActual = '';
+	var pathActualArray = [];
+	$scope.pathActual = " / ";
+
 
 	function addType(list){
 		angular.forEach(list, function(value, key){
 			res = value.fileType.split("/");
 			value.type = res[0];
 		});
+	};
+
+	function generatePath(newArrayPath){
+		pathActualArray = newArrayPath;
+		$scope.pathActual = "";
+		for(var key in pathActualArray){
+			$scope.pathActual = $scope.pathActual + "/ " + pathActualArray[key] + " ";
+		}
+	}
+
+	$scope.backPathActual = function(){
+		res = $scope.pathActual.trim().split(" / ");
+		res.splice(res.length - 1, 1);
+		for (var key in res){
+			$scope.pathActual = $scope.pathActual + res[key];
+		}
 	};
 
 	RequestHandler.get(api + "file/all")
@@ -28,30 +46,41 @@ app.controller('filesCtrl', function ($scope, $rootScope, RequestHandler, socket
 					$scope.treeSelected = result.data.data;
 					$scope.elementsActual = result.data.data;
 					addType($scope.elementsActual.fileList);
+					pathActualArray.push(result.data.data.name);
+					generatePath(pathActualArray);
 				});
 		}else if (value){
 			$scope.elementsActual = value;
 			addType($scope.elementsActual.fileList);
+			pathActualArray.push($scope.elementsActual.name);
+			generatePath(pathActualArray);
 		}
 	};
 
 
 	var folderBack;
-	$scope.backFolder = function(folder){
+	$scope.backFolder = function(folder, path){
+
+		if ($scope.treeBase == $scope.elementsActual){
+			return;
+		}
 
 		if ($scope.treeSelected == $scope.elementsActual){
 			$scope.treeSelected = '';
 			$scope.elementsActual = $scope.treeBase;
+			$scope.pathActual = "/";
 			return;
 		}
 
 		if(folder.path == $scope.elementsActual.path){
 			$scope.elementsActual = folderBack;
+			generatePath(path);
 		}else{
 			for (var key in folder.fileList){
 				if (folder.fileList[key].isDirectory){
 					folderBack = folder;
-					$scope.backFolder(folder.fileList[key]);
+					path.push(folder.name);
+					$scope.backFolder(folder.fileList[key], path);
 				}
 			}
 		}
