c6df3b5970d8dab4d0fd05881f114b2efff47ed7
Alexis COLLIN <alexis-collin@hotmail.fr>
2015-10-20 16:23:32 +0200

    Ajout de la taille et du bouton download pour les fichiers

--- seedApp/public/app/controllers/filesCtrl.js
+++ seedApp/public/app/controllers/filesCtrl.js
@@ -8,11 +8,20 @@ app.controller('filesCtrl', function ($scope, $rootScope, RequestHandler, socket
 	var pathActualArray = [];
 	$scope.pathActual = " / ";
 
+	function FileConvertSize(aSize){
+		aSize = Math.abs(parseInt(aSize, 10));
+		var def = [[1, 'octets'], [1024, 'ko'], [1024*1024, 'Mo'], [1024*1024*1024, 'Go'], [1024*1024*1024*1024, 'To']];
+		for(var i=0; i<def.length; i++){
+			if(aSize<def[i][0])
+				return (aSize/def[i-1][0]).toFixed(2)+' '+def[i-1][1];
+		}
+	}
 
 	function addType(list){
 		angular.forEach(list, function(value, key){
 			res = value.fileType.split("/");
 			value.type = res[0];
+			value.sizeConvert = FileConvertSize(value.size);
 		});
 	};
 
