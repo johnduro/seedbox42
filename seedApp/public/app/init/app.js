61709557a192bc4f7d9a5e43a4c4b80db6b56630
Alexis Collin <Alexis@iMac-de-Alexis.local>
2015-10-30 16:42:32 +0100

    Ajout des route privée avec verifications des droits Ajout de la page profile

--- seedApp/public/app/init/app.js
+++ seedApp/public/app/init/app.js
@@ -3,38 +3,46 @@ var app = angular.module('seedApp', ['ngRoute', 'ui.router', 'ngCookies', 'ngVid
 // ---------------------- variable global -------------------------------
 var api = "";
 
-app.run(function ($rootScope, $location, $http) {
-
-    if ($location.host() == "localhost"){
-        api = "http://localhost:3000/";
-    } else {
-        api = "http://37.187.111.179:3000/";
-    }
+app.run(function ($rootScope, $location, $http, $state, $location) {
 
     api = "/";
-
     $rootScope.token = localStorage.getItem("token");
-    console.log("TOKEN >> ", $rootScope.token);
     $rootScope.user = JSON.parse(localStorage.getItem("user"));
     $http.defaults.headers.common['X-Access-Token'] = $rootScope.token;
 
-
     $rootScope.tools = {
         convertSize: function (aSize){
-    		aSize = Math.abs(parseInt(aSize, 10));
-    		var def = [[1, 'octets'], [1024, 'ko'], [1024*1024, 'Mo'], [1024*1024*1024, 'Go'], [1024*1024*1024*1024, 'To']];
-    		for(var i=0; i<def.length; i++){
-    			if(aSize<def[i][0])
-    				return (aSize/def[i-1][0]).toFixed(2)+' '+def[i-1][1];
-    		}
-    	},
+            aSize = Math.abs(parseInt(aSize, 10));
+            var def = [[1, 'octets'], [1024, 'ko'], [1024 * 1024, 'Mo'], [1024 * 1024 * 1024, 'Go'], [1024 * 1024 * 1024 * 1024, 'To']];
+            for(var i = 0; i < def.length; i++){
+                if(aSize < def[i][0])
+                return (aSize / def[i-1][0]).toFixed(2) + ' ' + def[i-1][1];
+            }
+        },
         convertFields: function(list){
-    		angular.forEach(list, function(value, key){
-    			res = value.fileType.split("/");
-    			value.type = res[0];
-    			value.sizeConvert = $rootScope.tools.convertSize(value.size);
-    		});
-    	}
+            angular.forEach(list, function(value, key){
+                res = value.fileType.split("/");
+                value.type = res[0];
+                value.sizeConvert = $rootScope.tools.convertSize(value.size);
+            });
+        }
     };
+
+    $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
+
+        if ("access" in toState){
+            for (var key in toState.access){
+                if (key == $rootScope.user.role){
+                    return;
+                }
+            }
+            event.preventDefault();
+            if (fromState.name != "^"){
+                $location.url(fromState.url);
+            }else{
+                $location.url('seedbox/dashboard');
+            }
+        }
+    });
 
 });
--- seedApp/public/app/init/routes.js
+++ seedApp/public/app/init/routes.js
@@ -2,26 +2,40 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise("/seedbox/dashboard")
 
-    $stateProvider.state('connexion', {url: "/connexion",templateUrl: "app/indexConnexion.html", controller: "connexionCtrl"});
+    $stateProvider.state('connexion', {
+        url: "/connexion",
+        templateUrl: "app/indexConnexion.html",
+        controller: "connexionCtrl"}
+    );
     $stateProvider.state('seedbox', {url: "/seedbox",templateUrl: "app/indexSeedbox.html", controller: "seedboxCtrl"})
     .state('seedbox.dashboard', {
         url: "/dashboard",
         templateUrl: "app/views/dashboard.html",
-        controller: "dashboardCtrl"
+        controller: "dashboardCtrl",
+        access: ["0", "1"]
     })
-    .state('seedbox.users', {
-        url: "/users",
-        templateUrl: "app/views/users.html",
-        controller: "usersCtrl"
+    .state('seedbox.profile', {
+        url: "/profile",
+        templateUrl: "app/views/profile.html",
+        controller: "profileCtrl",
+        access: ["0", "1"],
     })
     .state('seedbox.torrents', {
         url: "/torrents",
         templateUrl: "app/views/torrents.html",
-        controller: "torrentsCtrl"
+        controller: "torrentsCtrl",
+        access: ["0", "1"]
     })
     .state('seedbox.files', {
         url: "/files",
         templateUrl: "app/views/files.html",
-        controller: "filesCtrl"
+        controller: "filesCtrl",
+        access: ["0", "1"]
+    })
+    .state('seedbox.adminUsers', {
+        url: "/admin/users",
+        templateUrl: "app/views/adminUsers.html",
+        controller: "usersCtrl",
+        access: ["0"]
     });
 });
