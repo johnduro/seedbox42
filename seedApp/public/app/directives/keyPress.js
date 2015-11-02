c800afac0c84922ac55d95312a5497a122946ab3
Alexis Collin <Alexis@iMac-de-Alexis.local>
2015-11-02 19:10:45 +0100

    Ajout de la directive err-src si l'image ne se charge pas

--- seedApp/public/app/directives/keyPress.js
+++ seedApp/public/app/directives/keyPress.js
@@ -38,3 +38,15 @@ app.directive('fileModel', ['$parse', function ($parse) {
         }
     };
 }]);
+
+app.directive('errSrc', function() {
+     return {
+       link: function(scope, element, attrs) {
+         element.bind('error', function() {
+           if (attrs.src != attrs.errSrc) {
+             attrs.$set('src', attrs.errSrc);
+           }
+         });
+       }
+     }
+   });
