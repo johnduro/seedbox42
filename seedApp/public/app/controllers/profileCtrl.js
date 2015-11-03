3d919b231092ea4dad00e8764d5ced0edc0f5b97
Alexis Collin <Alexis@iMac-de-Alexis.local>
2015-11-03 18:21:30 +0100

    update de la page profile

--- seedApp/public/app/controllers/profileCtrl.js
+++ seedApp/public/app/controllers/profileCtrl.js
@@ -3,44 +3,26 @@ app.controller("profileCtrl", function($scope, $rootScope, Upload){
     console.log("profileCtrl");
     console.log($rootScope.user);
 
-    if (!("avatar" in $rootScope)){
-        $rootScope.user.avatar = "undefined";
-    }
+    $scope.editUser = angular.copy($rootScope.user);
+    console.log($scope.editUser);
 
-    $scope.$watch('files', function () {
-        $scope.upload($scope.files);
-    });
-    $scope.$watch('file', function () {
-        if ($scope.file != null) {
-            $scope.files = [$scope.file];
-        }
-    });
+    $scope.updateUser = function(){
+        var send = {};
 
-    $scope.upload = function (files) {
-
-        if (files && files.length) {
-            for (var i = 0; i < files.length; i++) {
-              var file = files[i];
-              if (!file.$error) {
-                Upload.upload({
-                    url: '/users',
-                    data: {
-                      avatar: file,
-                      login: $rootScope.user.login
-                    }
-                }).progress(function (evt) {
-                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    $scope.log = 'progress: ' + progressPercentage + '% ' + evt.config.data.file.name + '\n' + $scope.log;
-                }).success(function (data, status, headers, config) {
-					/*if (!data[0].success){
-						$rootScope.msgInfo("Error !", "L'ajout du nouveau torrent a echoue...");
-					}*/
-                }).error(function (data, status, headers, config) {
-					$rootScope.msgInfo("Error !", "L'ajout du nouveau torrent a echoue...");
-				});
-              }
+        angular.forEach($rootScope.user, function(item1, index) {
+            if ($rootScope.user[index] != $scope.editUser[index]){
+                send[index] = $scope.editUser[index];
             }
-        }
-    };
+        });
+        console.log($rootScope.user);
+        console.log(send);
+		/*RequestHandler.put(api + "users/" + $rootScope.user._id, $scope.selectUser)
+			.then(function(result){
+				console.log(result);
+			});*/
+	};
 
+    /*if (!("avatar" in $rootScope)){
+        $rootScope.user.avatar = "undefined";
+    }*/
 });
