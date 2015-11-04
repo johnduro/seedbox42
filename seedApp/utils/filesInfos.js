7f93c2d37b5e13985913d25335abcec062aae35c
johnduro <mle-roy@student.42.fr>
2015-11-04 18:55:47 +0100

    debut de l ajout de dossier, test des droits

--- seedApp/utils/filesInfos.js
+++ seedApp/utils/filesInfos.js
@@ -82,14 +82,55 @@ module.exports = {
 
 	getDirInfos: function (path, done) {
 		var dirInfos = {};
+		var result = [];
+		console.log('Current uid: ' + process.getuid());
+		console.log('effective > ', process.geteuid());
+		console.log('Current gid: ' + process.getgid());
 		fs.stat(path, function (err, fStat) {
+			if (err)
+				done(err);
 			if (fStat.isDirectory())
 			{
-				done(null, dirInfos);
+				console.log('IS DIR > ', fStat);
+				fs.readdir(path, function (err, files) {
+					if (err)
+						done(err);
+					var i = 0;
+					(function next () {
+						var file = files[i++];
+						if (!file)
+							return done(null, result);
+						fs.open(path + '/' + file, 'a', function (err, fd) {
+							console.log("FILE > ", file);
+							if (err)
+							{
+								console.log('ERR OPEN >> ', err);
+								if (err.code == 'EACCES')
+									console.log("PAS LES DROIITS !!!!");
+							}
+							next();
+						});
+						// fs.stat(path + '/' + file, function (err, stats) {
+						// 	console.log('NAME > ', file);
+						// 	console.log('    owner eXecute:  ' + (stats["mode"] & 100 ? 'x' : '-'));
+						// 	console.log('    owner Write:    ' + (stats["mode"] & 200 ? 'w' : '-'));
+						// 	console.log('    owner Read:     ' + (stats["mode"] & 400 ? 'r' : '-'));
+						// 	console.log('    group eXecute:  ' + (stats["mode"] & 10 ? 'x' : '-'));
+						// 	console.log('    group Write:    ' + (stats["mode"] & 20 ? 'w' : '-'));
+						// 	console.log('    group Read:     ' + (stats["mode"] & 40 ? 'r' : '-'));
+						// 	console.log('    others eXecute: ' + (stats["mode"] & 1 ? 'x' : '-'));
+						// 	console.log('    others Write:   ' + (stats["mode"] & 2 ? 'w' : '-'));
+						// 	console.log('    others Read:    ' + (stats["mode"] & 4 ? 'r' : '-'));
+						// });
+					})();
+					console.log("DIR INFOS > ", files);
+				});
+				// done(null, fStat);
 			}
 			else
 			{
-				done(null, dirInfos);
+				console.log('IS NOT DIR > ', fStat);
+				// done(null, fStat);
 			}
 		});
 	}
