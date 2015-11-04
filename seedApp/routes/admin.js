51a2fc0fed46d392b80dd5908092ae988ebbc17f
johnduro <mle-roy@student.42.fr>
2015-11-04 16:33:33 +0100

    ajout du put transmission, transmissionsettings et torrent

--- seedApp/routes/admin.js
+++ seedApp/routes/admin.js
@@ -1,17 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var TransmissionNode = require('../utils/transmissionNode');
 
 router.get('/settings', function (req, res, next) {
 	var config = req.app.get('config');
 	var ret = {
 		"transmission": config.transmission,
 		"transmission-settings": config["transmission-settings"],
+		"torrents": config.torrents,
 		"files": config.files,
 		"dashboard": config.dashboard,
 		"users": config.users
 	};
 	res.json({ success: true, data: ret });
+});
+
+router.put('/settings/transmission', function (req, res, next) {
+	if (req.user.role == 0 && Object.keys(req.body).length)
+	{
+		var transmissionConfig = req.app.get('config').transmission;
+		var mod = false;
+		for (var key in req.body)
+		{
+			if (transmissionConfig.hasOwnProperty(key) && transmissionConfig[key] != req.body[key])
+			{
+				transmissionConfig[key] = req.body[key];
+				mod = true;
+			}
+		}
+		if (mod)
+		{
+			var newTransmission = new TransmissionNode(transmission);
+			newTransmission.sessionGet(function (err, res) {
+				if (err)
+					res.json({ success: false, message: "could not change transmission settings" });
+				else
+				{
+					req.app.get('config').transmission = transmissionConfig;
+					req.app.set('transmission', newTransmission);
+					// WRITE TO FILE
+					res.json({ success: true, message: "transmission infos were successfuly updated" });
+				}
+			});
+		}
+		else
+			res.json({ success: false, message: "no changes were made" });
+	}
+	else
+		res.json({ success: false, message: "You don't have enought rights for this action" });
 });
 
 router.put('/settings/transmission-settings', function (req, res, next) {
@@ -35,7 +72,7 @@ router.put('/settings/transmission-settings', function (req, res, next) {
 			{
 				transmission.sessionGet(function (err, res) {
 					if (err)
-						res.json({ success: true, message: "transmission settings succesfully updated, but could not get session infos from transmission" });
+						res.json({ success: true, message: "transmission settings succesfully updated, but could not get session infos from transmission", data: null });
 					else
 					{
 						for (var key in tSettings)
@@ -47,12 +84,30 @@ router.put('/settings/transmission-settings', function (req, res, next) {
 							}
 						}
 						req.app.get('config')["transmission-settings"] = tSettings;
+						// WRITE TO FILE
 						res.json({ success: true, message: "transmission settings succesfully updated", data: tSettings });
 					}
 				});
 			}
 				res.json({ success: true, message: "transmission settings succesfully updated", data: res });
 		});
+	}
+	else
+		res.json({ success: false, message: "You don't have enought rights for this action" });
+});
+
+router.put('/settings/torrents', function (req, res, next) {
+	if (req.user.role == 0)
+	{
+		var trSettings = req.app.get('config').torrents;
+		for (var key in req.body)
+		{
+			if (trSettings.hasOwnProperty(key) && trSettings[key] != req.body[key])
+				trSettings[key] = req.body[key];
+		}
+		req.app.get('config').torrents = trSettings;
+		// WRITE TO FILE
+		res.json({ success: true, message: "torrents settings succesfully updated" });
 	}
 	else
 		res.json({ success: false, message: "You don't have enought rights for this action" });
