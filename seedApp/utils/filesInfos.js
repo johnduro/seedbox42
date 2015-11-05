d7636695378fef3e43a5d09d8555f4bcfa3e79a4
johnduro <mle-roy@student.42.fr>
2015-11-05 04:26:34 +0100

    outils pour recuperer les droits des fichiers ok, faire plus de test

--- seedApp/utils/filesInfos.js
+++ seedApp/utils/filesInfos.js
@@ -1,6 +1,91 @@
 var fs = require('fs');
 var mime = require('mime');
 
+
+
+var getFileRights = function (path, done) {
+	// fs.access(path, fs.F_OK, function (err) {
+		// console.log("FILE > ", file);
+		// if (err)
+		// 	console.log("ERROR F > ", err);
+	var rights = {
+		read: true,
+		write: true
+	};
+	fs.access(path, fs.R_OK, function (err) {
+		if (err)
+			rights.read = false;
+			// console.log("ERROR R > ", err);
+		fs.access(path, fs.W_OK, function (err) {
+			if (err)
+				rights.write = false;
+			// console.log("ERROR W > ", err);
+			done(rights);
+		});
+	});
+	// });
+
+};
+
+var getFileRightsRecurs = function self (path, done) {
+	var infos = {
+		fileType: '',
+		rights : {
+			read: true,
+			write: true
+		},
+		size: 0,
+		path: path
+	};
+	// var rights = {
+	// 	read: true,
+	// 	write: true
+	// };
+	fs.stat(path, function (err, fStat) {
+		if (err)
+			done(err);
+		getFileRights(path, function (pRights) {
+			// rights.read = pRights.read ? rights.read : pRights.read; //refaire ternaire inutile
+			// rights.write = pRights.write ? rights.write : pRights.write; //refaire ternaire inutile
+			infos.rights.read = pRights.read;
+			infos.rights.write = pRights.write;
+			infos.fileType = mime.lookup(path);
+			infos.size += fStat.size;
+			if (fStat.isDirectory() && infos.rights.read)
+			{
+				infos.fileType = 'folder';
+				fs.readdir(path, function (err, files) {
+					if (err)
+						return done(err);
+					var i = 0;
+					(function next () {
+						var file = files[i++];
+						if (!file)
+							return done(null, infos);
+							// return done(null, rights);
+						var filePath = path + '/' + file;
+						// self(filePath, function (err, fRights) {
+						self(filePath, function (err, fInfos) {
+							if (err)
+								done(err);
+							// rights.read = fRights.read ? rights.read : fRights.read;
+							// rights.write = fRights.write ? rights.write : fRights.write;
+							infos.rights.read = fInfos.rights.read ? infos.rights.read : fInfos.rights.read;
+							infos.rights.write = fInfos.rights.write ? infos.rights.write : fInfos.rights.write;
+							infos.size += fInfos.size;
+							next();
+						});
+					})();
+				});
+			}
+			else
+				// done(null, pRights);
+				done(null, infos);
+		});
+	});
+};
+
+
 /**
  * Files Infos
  */
@@ -19,7 +104,7 @@ module.exports = {
 	},
 
 	getFileInfosRecurs: function self (path, name, done) {
-		var fileInfos = { name: name, path: path, isDirectory: false, size: 0, fileList: [] };
+		var fileInfos = { name: name, path: path, isDirectory: false, size: 0, fileList: [] }; //isDirectory utile ?
 		fs.stat(path, function (err, fileStats) {
 			if (err)
 				return done(err);
@@ -80,12 +165,13 @@ module.exports = {
 		}
 	},
 
+
 	getDirInfos: function (path, done) {
-		var dirInfos = {};
+		// var dirInfos = {};
 		var result = [];
-		console.log('Current uid: ' + process.getuid());
-		console.log('effective > ', process.geteuid());
-		console.log('Current gid: ' + process.getgid());
+		// console.log('Current uid: ' + process.getuid());
+		// console.log('effective > ', process.geteuid());
+		// console.log('Current gid: ' + process.getgid());
 		fs.stat(path, function (err, fStat) {
 			if (err)
 				done(err);
@@ -100,28 +186,17 @@ module.exports = {
 						var file = files[i++];
 						if (!file)
 							return done(null, result);
-						fs.open(path + '/' + file, 'a', function (err, fd) {
-							console.log("FILE > ", file);
+						// getFileRightsRecurs(path + '/' + file, function (err, rights) {
+						getFileRightsRecurs(path + '/' + file, function (err, infos) {
 							if (err)
-							{
-								console.log('ERR OPEN >> ', err);
-								if (err.code == 'EACCES')
-									console.log("PAS LES DROIITS !!!!");
-							}
+								done(err);
+
+								// console.log(err);
+							// console.log('FILE > ', file);
+							// console.log('RIGHTS > ', rights);
+							result.push(infos);
 							next();
 						});
-						// fs.stat(path + '/' + file, function (err, stats) {
-						// 	console.log('NAME > ', file);
-						// 	console.log('    owner eXecute:  ' + (stats["mode"] & 100 ? 'x' : '-'));
-						// 	console.log('    owner Write:    ' + (stats["mode"] & 200 ? 'w' : '-'));
-						// 	console.log('    owner Read:     ' + (stats["mode"] & 400 ? 'r' : '-'));
-						// 	console.log('    group eXecute:  ' + (stats["mode"] & 10 ? 'x' : '-'));
-						// 	console.log('    group Write:    ' + (stats["mode"] & 20 ? 'w' : '-'));
-						// 	console.log('    group Read:     ' + (stats["mode"] & 40 ? 'r' : '-'));
-						// 	console.log('    others eXecute: ' + (stats["mode"] & 1 ? 'x' : '-'));
-						// 	console.log('    others Write:   ' + (stats["mode"] & 2 ? 'w' : '-'));
-						// 	console.log('    others Read:    ' + (stats["mode"] & 4 ? 'r' : '-'));
-						// });
 					})();
 					console.log("DIR INFOS > ", files);
 				});
@@ -129,7 +204,15 @@ module.exports = {
 			}
 			else
 			{
-				console.log('IS NOT DIR > ', fStat);
+				// getFileRightsRecurs(path, function (err, rights) {
+				getFileRightsRecurs(path, function (err, infos) {
+					if (err)
+						done(err);
+						// console.log(err);
+					// console.log('IS NOT DIR > ', fStat);
+					done(null, infos);
+					// done(null, [{ size: fStat.size, rights: rights, path: path }]);
+				});
 				// done(null, fStat);
 			}
 		});
