bcb483f16fbee885873cd52ac1fe1114bee1dbed
johnduro <mle-roy@student.42.fr>
2015-11-10 14:47:15 +0100

    modification de l envois des nouveaux fichiers vers le serveur

--- seedApp/public/app/controllers/directoryCtrl.js
+++ seedApp/public/app/controllers/directoryCtrl.js
@@ -8,7 +8,8 @@ app.controller("directoryCtrl", function($rootScope, $scope, RequestHandler, Too
 
     $scope.selectItem = function(index){
         var a;
-        if ((a = indexSelected.indexOf(index)))
+        // if ((a = indexSelected.indexOf(index)))  // NULL !
+        if ((a = indexSelected.indexOf(index)) == -1)
             indexSelected.push(index);
         else
             indexSelected.splice(a, 1);
@@ -19,16 +20,27 @@ app.controller("directoryCtrl", function($rootScope, $scope, RequestHandler, Too
         RequestHandler.get(api + "admin/new-directory/" + btoa($scope.sendDir))
             .then(function(result){
                 if (result.data.success)
+				{
+					console.log("result::get:: ", result.data.data);
                     $scope.tree = result.data.data;
+				}
             });
     };
 
     $scope.addDirectory = function(){
         var send = [];
-        if (indexSelected.length){
-            for (var key in indexSelected){
-                send.push($scope.tree[key]);
-            }
+        if (indexSelected.length)
+		{
+            // for (var key in indexSelected){ //SERIEUSEMENT ?!
+            //     send.push($scope.tree[key]);
+            // }
+			for (var i = 0; i < indexSelected.length; i++)
+			{
+				var key = indexSelected[i];
+				send.push($scope.tree[key]);
+			}
+			console.log('tree::put:: ', $scope.tree);
+			console.log('send::put:: ', send);
             RequestHandler.put(api + "admin/new-directory", send)
                 .then(function(result){
                     console.log(result.data);
