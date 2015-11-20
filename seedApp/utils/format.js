74ef6fbdf6da234cb210b3183c605c401e6f68ff
johnduro <mle-roy@student.42.fr>
2015-11-20 16:17:59 +0100

    fin de script pour ajouter des fichier en db depuis la ligne de commande

--- seedApp/utils/format.js
+++ seedApp/utils/format.js
@@ -1,4 +1,6 @@
+var chalk = require('chalk');
 var User = require('../models/User');
+var ft = require('../utils/ft');
 
 module.exports = {
 	fileList: function (files, user) {
@@ -61,5 +63,34 @@ module.exports = {
 			ret.push(parseInt(id, 10));
 		});
 		return (ret);
+	},
+
+	managerAddDirectory: function (files) {
+		var sizeMax = 0;
+		var pathMax = 0;
+		var typeMax = 0;
+		var filesObj = {};
+		var choices = [];
+		var writeWarning = "You may not have the right to write/delete this file";
+		var readWarning = "You may not have the right to read this file";
+		files.forEach(function (file) {
+			file.sizeStr = ft.convertSize(file.size);
+			sizeMax = ( file.sizeStr.length > sizeMax ) ? file.sizeStr.length : sizeMax;
+			pathMax = ( file.path.length > pathMax ) ? file.path.length : pathMax;
+			typeMax = ( file.fileType.length > typeMax ) ? file.fileType.length : typeMax;
+		});
+		files.forEach(function (file) {
+			var name = "";
+			name += file.path + (' '.repeat(pathMax - file.path.length));
+			name += ' - ' + file.sizeStr + (' '.repeat(sizeMax - file.sizeStr.length));
+			name += ' - ' + file.fileType + (' '.repeat(typeMax - file.fileType.length));
+			if (file.rights.write == false)
+				name += (' - ' + chalk.yellow(writeWarning));
+			if (file.rights.read == false)
+				name += (' - ' + chalk.red(readWarning));
+			choices.push({ name: name });
+			filesObj[name] = file;
+		});
+		return ({ choices: choices, filesObj: filesObj });
 	}
 };
