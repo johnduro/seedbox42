afc9e868e715beea373aa312c54fa2d57764ab69
johnduro <mle-roy@student.42.fr>
2015-11-23 22:13:31 +0100

    nouvelle version du script fini, quelques modification a faire encore

--- seedApp/manager/conf-to-transmission.js
+++ seedApp/manager/conf-to-transmission.js
@@ -1,5 +1,14 @@
-
+var chalk = require('chalk');
+var tSettings = require('../config/transmission');
 
 module.exports = function (configFileName, args) {
-	console.log('WUT?!');
+	tSettings.configToTransmissionSettings(args.transmission, args.config['transmission-settings'], function (err) {
+		if (err)
+		{
+			console.log(chalk.red("Could not set transmission settings, please check configuration and/or transmission"));
+			process.exit();
+		}
+		else
+			console.log(chalk.green("Transmission was successfully updated from configuration file"));
+	});
 };
