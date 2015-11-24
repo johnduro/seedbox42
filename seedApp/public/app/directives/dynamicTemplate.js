47cdebcc1d911bde2afa4d09e11358dba58bb0bd
Alexis Collin <Alexis@iMac-de-Alexis.local>
2015-11-24 22:51:24 +0100

    Finalisation du dashboard avec les templates dynamic avec le order

--- seedApp/public/app/directives/dynamicTemplate.js
+++ seedApp/public/app/directives/dynamicTemplate.js
@@ -1,48 +1,38 @@
-app.directive('contentItem', function ($compile, RequestHandler, $rootScope) {
-    var getTemplate = function (templates, contentType) {
-        var template = '';
-
-        switch (contentType) {
-            case 'image':
-                template = templates.imageTemplate;
-                break;
-            case 'video':
-                template = templates.videoTemplate;
-                break;
-            case 'notes':
-                template = templates.noteTemplate;
-                break;
-        }
-
-        return template;
-    };
+app.directive('contentItem', function ($compile, RequestHandler, $rootScope, $location, socket) {
 
     var linker = function (scope, element, attrs) {
 
-        console.log(scope.content.type);
+        scope.messages = [];
 
-        RequestHandler.get("/app/views/partials/" + scope.content.type + ".html")
+        RequestHandler.get("/app/views/partials/" + scope.content.template + ".html")
             .then(function(resultTemplate){
-                RequestHandler.get(api + "dashboard/" + scope.content.name)
-            		.then(function(result){
-            			scope.lastFiles = result.data.data;
-                        
-            			$rootScope.tools.convertFields(scope.lastFiles);
-                        element.html(resultTemplate.data).show();
-                        $compile(element.contents())(scope);
-            		});
-                //console.log(result.data);
+                if (scope.content.name == "chat"){
+                    socket.emit("chat:get:message", null, function(data){
+                		scope.messages = data.message;
+                	});
+                }else{
+                    RequestHandler.get(api + "dashboard/" + scope.content.name)
+                		.then(function(result){
+                			scope.files = result.data.data;
+                			$rootScope.tools.convertFields(scope.files);
+                		});
+                }
 
+                element.html(resultTemplate.data).show();
+                $compile(element.contents())(scope);
             });
-        /*scope.rootDirectory = 'images/';
 
-        TemplateService.getTemplates().then(function (response) {
-            var templates = response.data;
 
-            element.html(getTemplate(templates, scope.content.content_type));
-
-            $compile(element.contents())(scope);
-        });**/
+        scope.openFile = function(file){
+    		$location.url('seedbox/file/' + file._id);
+    	};
+        scope.sendMessage = function(){
+    		socket.emit("chat:post:message", {message: scope.newMessage, id:$rootScope.user._id});
+    		scope.newMessage = "";
+    	};
+        socket.on("chat:post:message", function(data){
+    		scope.messages.push(data.newmessage);
+    	});
     };
 
     return {
