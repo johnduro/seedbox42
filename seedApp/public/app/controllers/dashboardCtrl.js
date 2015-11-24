baa87c88a9c70f17f1f1dd4df49efa8bb7fdc8b6
Alexis COLLIN <alexis-collin@hotmail.fr>
2015-11-24 22:51:24 +0100

    Finalisation du dashboard avec les templates dynamic avec le order

--- seedApp/public/app/controllers/dashboardCtrl.js
+++ seedApp/public/app/controllers/dashboardCtrl.js
@@ -5,16 +5,11 @@ app.controller('dashboardCtrl', function ($scope, $rootScope, $timeout, $locatio
 
 	$scope.newMessage = "";
 
-	$scope.openFile = function(file){
-		//console.log(file);
-		$location.url('seedbox/file/' + file._id);
-	};
-
-
 	$scope.content = [
-    {"name" : "recent-user-file", "enabled" : "all", "type" : "files-list", "title": "recent-user-file", "order": 0},
-    {"name" : "recent-file", "enabled" : "all", "type" : "files-list", "title": "recent-file", "order": 0},
-];
+	    {"name" : "recent-user-file", "enabled" : "all", "template" : "files-list", "title": "recent-user-file", "order": 3},
+	    {"name" : "recent-file", "enabled" : "all", "template" : "files-list", "title": "recent-file", "order": 1},
+		{"name" : "chat", "enabled" : "all", "template" : "dashboardChat", "title": "Mini chat", "order": 0}
+	];
 
 	RequestHandler.get(api + "dashboard/disk-space")
 		.then(function(result){
@@ -28,30 +23,4 @@ app.controller('dashboardCtrl', function ($scope, $rootScope, $timeout, $locatio
 			    };
 			}
 		});
-
-	RequestHandler.get(api + "dashboard/recent-user-file")
-		.then(function(result){
-			$scope.userLastFiles = result.data.data;
-			$rootScope.tools.convertFields($scope.userLastFiles);
-		});
-
-	RequestHandler.get(api + "dashboard/recent-file")
-		.then(function(result){
-			$scope.lastFiles = result.data.data;
-			$rootScope.tools.convertFields($scope.lastFiles);
-		});
-
-	socket.emit("chat:get:message", null, function(data){
-		$scope.messages = data.message;
-	});
-
-	$scope.sendMessage = function(){
-		socket.emit("chat:post:message", {message: $scope.newMessage, id:$rootScope.user._id});
-		$scope.newMessage = "";
-	};
-
-	socket.on("chat:post:message", function(data){
-		$scope.messages.push(data.newmessage);
-	});
-
 });
