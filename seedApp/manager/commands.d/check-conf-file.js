255f5fcb18d4a274ef2b34e357b4b74501bd996f
Alexis Collin <alexis-collin@hotmail.fr>
2016-01-06 14:15:17 +0100

    Suppression du dossier SeedApp

--- seedApp/manager/commands.d/check-conf-file.js
+++
@@ -1,8 +0,0 @@
-var configDefault = require('../../config/default-config');
-var validity = require('../../config/validity');
-
-module.exports = function (configFileName, args, commandLineArg, done) {
-	var valErr = validity.checkConfig(args.config, configDefault, '', configFileName);
-	validity.checkConfigErrors(valErr, configFileName);
-	return done();
-};
