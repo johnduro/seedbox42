f31a3b3a31f720cf32c58adf91380f1044a1aff8
Alexis Collin <alexis-collin@hotmail.fr>
2016-01-21 15:54:16 +0100

    Sorry

--- public/app/services/Tools.js
+++ public/app/services/Tools.js
@@ -263,7 +263,7 @@ app.service('Tools', function ($rootScope, RequestHandler, $timeout, $modal, $q)
             defer.resolve($rootScope.config);
         }else{
             RequestHandler.get(api + "admin/settings")
-                .then(function(result){);
+                .then(function(result){
                     if (result.data.status)
                         $rootScope.config = result.data.data;
                     defer.resolve(result.data.data);
