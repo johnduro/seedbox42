e1643c554eda4db6ef67104b912b3bd016d78bd2
johnduro <https://johnduro@bitbucket.org>
2016-08-11 14:58:01 +0200

    add alias for docker, link to mongodb to do

---
+++ docker/docker-alias.sh
@@ -0,0 +1,8 @@
+
+
+
+alias sbd-up='docker-compose up -d seed-node'
+
+alias sbd-nodemon='docker-compose run --rm seed-node nodemon'
+
+alias sbd-manager='docker-compose run --rm seed-node node ttManager.js'
